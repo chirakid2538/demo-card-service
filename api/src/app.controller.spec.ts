49cb9014907d03886fb2286d2aacd9ac9c29e275
chirakid kaweesirikon <chirakid.k@gmail.com>
2023-07-09 16:16:50 +0700

    :sparkles: add test handle-error class-validator
    added

--- api/src/app.controller.spec.ts
+++ api/src/app.controller.spec.ts
@@ -1,7 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ValidationError } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { hash } from './common/utils';
+import { convertError, hash } from './common/utils';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -35,6 +36,48 @@ describe('AppController', () => {
       const hashed = await hash.encode('123456789');
       const isCorrect = await hash.compare(')#421wdf32', hashed);
       expect(isCorrect).toBe(false);
+    });
+  });
+
+  describe('utils.handle-error', () => {
+    it('convertError return object', async () => {
+      const errors: ValidationError[] = [
+        {
+          property: 'name',
+          constraints: {
+            min: 'page must not be less than 1',
+            isInt: 'page must be an integer number',
+          },
+        },
+        {
+          property: 'address',
+          children: [
+            {
+              property: 'address',
+              constraints: {
+                isString: 'address must not be string',
+              },
+            },
+            {
+              property: 'phone',
+              constraints: {
+                isString: 'phone must not be string',
+              },
+            },
+          ],
+        },
+      ];
+      const expected = {
+        name: {
+          min: 'page must not be less than 1',
+          isInt: 'page must be an integer number',
+        },
+        address: {
+          address: { isString: 'address must not be string' },
+          phone: { isString: 'phone must not be string' },
+        },
+      };
+      expect(convertError({}, errors)).toMatchObject(expected);
     });
   });
 });
