f18c6af6cfde20a0a785fa38b8d6ae8009a8b206
chirakid kaweesirikon <chirakid.k@gmail.com>
2023-07-09 16:05:25 +0700

    :sparkles: add util hash test func
    added

--- api/src/app.controller.spec.ts
+++ api/src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { hash } from './common/utils';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -17,6 +18,23 @@ describe('AppController', () => {
   describe('root', () => {
     it('should return works', () => {
       expect(appController.getHello()).toBe('works');
+    });
+  });
+
+  describe('utils.hash', () => {
+    it('encode return string', async () => {
+      const hashed = await hash.encode('123456789');
+      expect(typeof hashed).toBe('string');
+    });
+    it('compare is valid return true', async () => {
+      const hashed = await hash.encode('123456789');
+      const isCorrect = await hash.compare('123456789', hashed);
+      expect(isCorrect).toBe(true);
+    });
+    it('compare is invalid return false', async () => {
+      const hashed = await hash.encode('123456789');
+      const isCorrect = await hash.compare(')#421wdf32', hashed);
+      expect(isCorrect).toBe(false);
     });
   });
 });
